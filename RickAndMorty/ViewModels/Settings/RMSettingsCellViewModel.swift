eb1651089160dd25c89eeb1076eac579aa196ca6
Dias <dias.atudinov@gmail.com>
2025-05-01 22:08:06 +0500

    e30

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsCellViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  RMSettingsCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dias Atudinov on 22.04.2025.
+//
+ 
+import UIKit
+
+struct RMSettingsCellViewModel: Identifiable, Hashable {
+    let id = UUID()
+    
+    private let type: RMSettingsOption
+    
+    // MARK: - Init
+    
+    init(type: RMSettingsOption) {
+        self.type = type
+    }
+    
+    // MARK: - Public
+    
+    public var image: UIImage? {
+        return type.iconImage
+    }
+    
+    public var title: String {
+        return type.displayTitle
+    }
+    
+    public var iconContainerColor: UIColor {
+        return type.iconContainerColor
+    }
+}
---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dias Atudinov on 22.04.2025.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+    
+   
+}
